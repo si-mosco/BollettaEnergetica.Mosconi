d439e831ee992e0daafa7b2e5c4f38f4ae48f536
mosconi.19322 <mosconi.19322@LABI1-6PC10>
2023-01-09 08:33:54 +0100

    Aggiungere i file di progetto.

---
+++ BollettaEnergetica.Mosconi/BollettaEnergetica.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BollettaEnergetica.Mosconi
+{
+    public class BollettaEnergetica
+    {
+        //attributi
+        private string _id;
+        private double _prezunita;
+        private double _prezdistribuzione;
+        private double _consumo;
+        private double _tasse;
+        private double _perctassa;
+
+        //costruttori
+        public string Id
+        {
+            get
+            {
+                return _id;
+            }
+            set
+            {
+                _id = value;
+            }
+        }
+        public double Prezunita
+        {
+            get
+            {
+                return _prezunita;
+            }
+            set
+            {
+                _prezunita = value;
+            }
+        }
+        public double Prezdistribuzione
+        {
+            get
+            {
+                return _prezdistribuzione;
+            }
+            set
+            {
+                _prezdistribuzione = value;
+            }
+        }
+        public double Consumo
+        {
+            get
+            {
+                return _consumo;
+            }
+            set
+            {
+                _consumo = value;
+            }
+        }
+        public double Tasse
+        {
+            get
+            {
+                return _tasse;
+            }
+            set
+            {
+                _tasse = value;
+            }
+        }
+        public double Perctassa
+        {
+            get
+            {
+                return _perctassa;
+            }
+            set
+            {
+                _perctassa = value;
+            }
+        }
+
+        //metodi
+    }
+}
